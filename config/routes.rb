3d024160261e6a110b741a885e462d41a1174170
James Dindi <dindijjames@gmail.com>
2017-12-25 05:43:37 +0300

    Create posts and pages controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # Index or root route
+  root 'posts#index'
+
+  # Regular route
+  get 'about' => 'pages#about'
+
+  # Resources route
+  resources :posts
 end
