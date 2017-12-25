8cf641ee1c95e815dd8553cd96b74f593c5d4bb0
James Dindi <dindijjames@gmail.com>
2017-12-25 07:34:16 +0300

    Add Bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # Index or root route
-  root 'posts#index'
+  root 'posts#index', as: 'home'
 
   # Regular route
-  get 'about' => 'pages#about'
+  get 'about' => 'pages#about', as: 'about'
 
   # Resources route
   resources :posts
